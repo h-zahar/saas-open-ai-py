d51add64353b10e741070aaba323668f0ddcd91f
h-zahar <hz.rifat@gmail.com>
2022-11-18 06:13:36 +0600

    Added Validation and Sliced Output

--- app/main.py
+++ app/main.py
@@ -2,11 +2,29 @@ import os
 import openai
 import argparse
 
+MAX_LENGTH = 25
+
 # Load API key from an environment variable or secret management service
 def load_api_key():
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def validate_input(user_input):
+    if len(user_input) > 5:
+        print(f"Max Length Allowed: {MAX_LENGTH}")
+        exit()
+
+def show_snippet(response):
+    if (response["choices"][0]["finish_reason"] != "stop"):
+        snippet = response["choices"][0]["text"][2:] + "..."
+        print(f"\nSnippet: {snippet}")
+    else:
+        snippet = response["choices"][0]["text"][2:]
+        print(f"\nSnippet: {snippet}")
+
 def generate_snippet(user_input):
+    validate_input(user_input)
+    print(f"Value: {user_input}")
+    print("Generating snippet...")
     load_api_key()
 
     subject = user_input
@@ -15,7 +33,7 @@ def generate_snippet(user_input):
 
     response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=100)
 
-    print(response)
+    show_snippet(response)
 
 def get_input():
     parse = argparse.ArgumentParser()
