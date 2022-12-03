3dae2ab967dc12a846f55f70628874bfc4ee3c42
h-zahar <hz.rifat@gmail.com>
2022-12-03 07:10:05 +0600

    Handled CORS Policy

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from app.main import generate_snippet
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
